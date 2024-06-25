2166ff2395b48beb7f1772a64f62e1ad3ee81ffe
ilhamyon <ilhamyon@gmail.com>
2024-06-25 18:53:25 +0700

    add project

---
+++ data/data_kunjungan.js
@@ -0,0 +1,53 @@
+var json_data_kunjungan = 
+{
+  "type": "FeatureCollection",
+  "name": "data_kunjungan",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": "1",
+        "Nama Relawan": "Zulkifli",
+        "Alamat tujuan": "Desa Labulia",
+        "Nama yang dikunjungi": "M Andrian",
+        "Keterangan kunjungan": "Penyuluhan",
+        "Y": -8.6922499,
+        "X": 116.1628263,
+        "Foto": "Foto/SMKN 1 Kota Malang.jpeg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          116.1891142,
+          -8.6860711
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ID": "2",
+        "Nama Relawan": "Herdian",
+        "Alamat tujuan": "Desa Nyerot",
+        "Nama yang dikunjungi": "Muhsinin",
+        "Keterangan kunjungan": "Penyuluhan",
+        "Y": -8.6807382,
+        "X": 116.2226376,
+        "Foto": "Foto_SMK PGRI 2 Malang.jpeg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          116.2275085,
+          -8.6807382
+        ]
+      }
+    }
+  ]
+}
