786303813990c9286fbfaf7673790b23735ec294
ilhamyon <ilhamyon@gmail.com>
2024-06-26 15:33:26 +0700

    update map

--- data/data_kunjungan.js
+++ data/data_kunjungan.js
@@ -1,25 +1,15 @@
 var json_data_kunjungan = 
 {
-  "type": "FeatureCollection",
-  "name": "data_kunjungan",
-  "crs": {
-    "type": "name",
-    "properties": {
-      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-    }
-  },
   "features": [
     {
       "type": "Feature",
       "properties": {
-        "ID": "1",
-        "Nama Relawan": "Zulkifli",
-        "Alamat tujuan": "Desa Labulia",
-        "Nama yang dikunjungi": "M Andrian",
-        "Keterangan kunjungan": "Penyuluhan",
-        "Y": -8.6922499,
-        "X": 116.1628263,
-        "Foto": "Foto/SMKN 1 Kota Malang.jpeg"
+        "id": 1,
+        "namaRelawan": "Zulkifli",
+        "alamatTujuan": "Desa Labulia",
+        "namaYangDikunjungi": "M Andrian",
+        "keteranganKunjungan": "Penyuluhan",
+        "foto": "Foto/SMKN 1 Kota Malang.jpeg"
       },
       "geometry": {
         "type": "Point",
@@ -32,14 +22,12 @@ var json_data_kunjungan =
     {
       "type": "Feature",
       "properties": {
-        "ID": "2",
-        "Nama Relawan": "Herdian",
-        "Alamat tujuan": "Desa Nyerot",
-        "Nama yang dikunjungi": "Muhsinin",
-        "Keterangan kunjungan": "Penyuluhan",
-        "Y": -8.6807382,
-        "X": 116.2226376,
-        "Foto": "Foto_SMK PGRI 2 Malang.jpeg"
+        "id": 2,
+        "namaRelawan": "Herdian",
+        "alamatTujuan": "Desa Nyerot",
+        "namaYangDikunjungi": "Muhsinin",
+        "keteranganKunjungan": "Penyuluhan",
+        "foto": "Foto_SMK PGRI 2 Malang.jpeg"
       },
       "geometry": {
         "type": "Point",
